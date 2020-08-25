a1b54f93040d408de615b044c9bd7bf2b532cb0c
linuxbytes <real8686@gmail.com>
2020-08-24 22:11:25 -0400

    first build

---
+++ build/precache-manifest.59c47e5efea5b0cd18f6c6bc7d22159c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed4a19f58e27b21e4edc85a18bf6bfcc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c3f908af1403d1e599a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9d06cc764a8a5c6c430e",
+    "url": "/static/js/2.3c7fac71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3c7fac71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c3f908af1403d1e599a",
+    "url": "/static/js/main.d9938fd0.chunk.js"
+  },
+  {
+    "revision": "bf250259d9fd61dc7dbe",
+    "url": "/static/js/runtime-main.ecec8c93.js"
+  }
+]);
