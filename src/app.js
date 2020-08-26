ce8d8ab10544c33d30efb16d5293039f28b359bb
linuxbytes <real8686@gmail.com>
2020-08-25 22:06:52 -0400

    working props and states

--- src/app.js
+++ src/app.js
@@ -5,16 +5,52 @@ import Item from "./Item.js";
 
 import "./App.css";
 
-function App() {
-  return (
-    <div>
-      <Header />
+class App extends React.Component {
 
-      <ol>
-        <Item done={false} text={"First Todo"} />
-      </ol>
-    </div>
-  );
+constructor(props){
+
+ super(props);
+    this.state = {
+      items: [
+        {
+          text: "First Todo",
+          done: false,
+        },
+        {
+          text: "Second Todo",
+          done: true,
+        },
+      ],
+    };
+
 }
+
+render() {
+    return (
+      <div>
+        <Header />
+
+        <ol>
+          {this.state.items.map((item) => (
+            <Item key={item.id} done={item.done} text={item.text} />
+          ))}
+        </ol>
+      </div>
+    );
+  }
+
+}
+
+// function App() {
+//   return (
+//     <div>
+//       <Header />
+
+//       <ol>
+//         <Item done={false} text={"First Todo"} />
+//       </ol>
+//     </div>
+//   );
+// }
 
 export default App;
